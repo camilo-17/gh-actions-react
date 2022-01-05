803670faec93aabc70cbe82634fdf014932c7e39
camilo-17 <andresccamilo17@gmail.com>
2022-01-04 22:21:46 -0500

    [BUG] fix again route

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'https://https://camilo-17.github.io/gh-actions-react',
+    base: 'https://https://camilo-17.github.io/gh-actions-react/',
     plugins: [react()],
 });
