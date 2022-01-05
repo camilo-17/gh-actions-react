2adc72491d37ef76366eb150f13d8152e5791d9f
camilo-17 <andresccamilo17@gmail.com>
2022-01-04 22:05:59 -0500

    [DEV] delete strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-<<<<<<< HEAD
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
->>>>>>> 045e8860fa96b26270d7c62e9710acb75f59ba0a
