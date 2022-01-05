d408d8f658ad0e4afd1327765ce6fee8515d0c73
camilo-17 <andresccamilo17@gmail.com>
2022-01-05 00:08:43 -0500

    [DEV] more animation and re position on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { fadeInLeft, fadeInDown, fadeInUp, fadeInRight } from 'react-animations';
+import { fadeInLeft, fadeInDown, fadeInUp, fadeInRight, zoomIn } from 'react-animations';
 import { StyleSheet, css } from 'aphrodite';
 
 import './styles/Hero.css';
@@ -22,15 +22,22 @@ const styles = StyleSheet.create({
         animationName: fadeInRight,
         animationDuration: '5s',
     },
+    zoomIn: {
+        animationName: zoomIn,
+        animationDuration: '5s',
+    },
 });
 
 function Hero() {
     return (
         <div className="play-container">
-            <img className={css(styles.fadeInLeft)} src="./assets/triangle.png" alt="triangle" />
-            <img className={css(styles.fadeInUp)} src="./assets/square.png" alt="triangle" />
-            <img className={css(styles.fadeInDown)} src="./assets/circle.png" alt="triangle" />
-            <img className={css(styles.fadeInRight)} src="./assets/ex.png" alt="triangle" />
+            <div className="images-play-container">
+                <img className={css(styles.fadeInLeft)} src="./assets/triangle.png" alt="triangle" />
+                <img className={css(styles.fadeInUp)} src="./assets/square.png" alt="triangle" />
+                <img className={css(styles.fadeInDown)} src="./assets/circle.png" alt="triangle" />
+                <img className={css(styles.fadeInRight)} src="./assets/ex.png" alt="triangle" />
+            </div>
+            <h3 className={css(styles.zoomIn)}>Status playing ...</h3>
         </div>
     );
 }
